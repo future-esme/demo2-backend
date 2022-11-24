0cf6c915c8b3ff2c9af5a30306013e1d9c68ed2d
Tulbu Gabriela <carte7respect@gmail.com>
2022-11-24 21:17:32 +0200

    another eror

--- src/main/java/utm/idw/web/rest/UserResource.java
+++ src/main/java/utm/idw/web/rest/UserResource.java
@@ -85,7 +85,7 @@ public class UserResource {
 
     private final UserRepository userRepository;
 
-    private final MailSrvice mailService;
+    private final MailSrvce mailService;
 
     public UserResource(UserService userService, UserRepository userRepository, MailService mailService) {
         this.userService = userService;
